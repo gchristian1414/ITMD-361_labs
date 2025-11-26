d3a79d68f7c5978ee727612ed91713b624c197dc
gchristian1414 <113937961+gchristian1414@users.noreply.github.com>
2025-11-26 02:49:42 -0600

    Update map.js

--- project_3/map.js
+++ project_3/map.js
@@ -58,7 +58,7 @@ function initMap() {
     });
 };
 
-google.maps.event.addDomListener(window, 'load', initMap);
+// google.maps.event.addDomListener(window, 'load', initMap);
 
 document.addEventListener("DOMContentLoaded", () => {
     const questionBtn = document.getElementById("questionButton");
